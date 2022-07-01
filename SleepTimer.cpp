87fd0eb7361b1a4e987465662a67b1d1d8d7b8ff
Shikami <106483519+Sh1kami@users.noreply.github.com>
2022-07-02 00:54:39 +0300

    Add files via upload

---
+++ SleepTimer.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	system("color 9");
+	system("title Sleep Timer");
+	setlocale(0, "");
+	int hour, min, sec = 0;
+	cout << endl << endl << "\tSleep Timer" << endl << endl << "\tВведите время через которое нужно выключить компьютер" << endl << endl;
+	cout << "\t\tЧасы: "; cin >> hour;
+	cout << "\t\tМинуты: "; cin >> min;
+	sec = (hour * 3600) + (min * 60);
+	cout << endl << endl;
+	long start = clock();
+	long tick;
+	while (0 < sec)
+	{
+		if ((((tick = clock()) - start) / CLOCKS_PER_SEC) == 1)
+		{
+			if (sec <= 60) system("color c");
+			cout << "\tДо выключение компьютера осталось (секунд): " << sec << endl;
+			sec--;
+			start = tick;
+		}
+	}
+	system("shutdown -s -t 1");
+}
